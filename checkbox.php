a1d2b6236ed79efd1d83bf3ed69c50cb7d03b6de
Stepanov Artur <89976322+satret@users.noreply.github.com>
2022-09-23 01:33:54 +0300

    Add files via upload

--- checkbox.php
+++ checkbox.php
@@ -1,14 +1,21 @@
 <?php
 session_start();
 date_default_timezone_set('Europe/Moscow');
+if (empty($_SESSION['k'])) {
+    $_SESSION['k'] = 1;
+} else {
+    $_SESSION['k']++;
+}
+$k = $_SESSION['k'];
+if (!isset($_SESSION['table']))
+    $_SESSION['table'] = array();
 $TIME = date_default_timezone_get();
 $startTime = microtime(true);
-//echo session_id();
 function validateFormat($x, $y, $r): bool
 {
     if (!($x == '-3' || $x == '-2' || $x == '-1' || $x == '0' || $x == '1' || $x == '2' || $x == '3' || $x == '4' || $x == '5') && is_numeric($y))
         return false;
-    if ($y > 5 || $y < -5 && is_numeric($y) && strlen($y)<=18)
+    if ($y > 5 || $y < -5 && is_numeric($y) && strlen($y) <= 18)
         return false;
     if ($r > 5 || $r < 1 && is_numeric($y))
         return false;
@@ -33,37 +40,178 @@ function answer($x, $y, $r): bool
     return false;
 }
 
+
 $x = $_GET["x"];
 $y = $_GET["y"];
 $r = $_GET["r"];
-if(!is_numeric($y) || !is_numeric($y) || !is_numeric($y)){
+if (!is_numeric($y) || !is_numeric($y) || !is_numeric($y)) {
     echo "number_is_not_numeric";
-}
-else {
-    $startTime = $_GET["startTime"];
-    $timeZone = $_GET["timeZone"];
+} else {
+    echo $k, '!';
     $validate = validateFormat($x, $y, $r);
-    $_SESSION['shot'] = false;
     if ($validate)
         $answer = answer($x, $y, $r);
     else
         $answer = false;
-
-
     $date = date('d.m.Y H:i:s', time());
-    $time = round(microtime(true) * 1000 - $startTime, 6) / 1000;
+    $time = round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 7);
+//    echo $x, '/';
+//    echo $y, '/';
+//    echo $r, '/';
     if ($answer) {
-        echo "1/";
+        $answer = "Попал";
+//        echo $answer, '/';
     } else {
-        echo "0/";
+        $answer = "Не попал";
+//        echo $answer, '/';
     }
-    echo $x, '/';
-    echo $y, '/';
-    echo $r, '/';
-    echo $date, '/';
-    echo $time;
-}
+//    echo $date, '/';
+//    echo $time, ';';
 
+
+//    $_SESSION['table'][$k][0] = $x;
+//    $_SESSION['table'][$k][1]  = $y;
+//    $_SESSION['table'][$k][2]  = $r;
+//    $_SESSION['table'][$k][3]  = $answer;
+//    $_SESSION['table'][$k][4]  = $date;
+//    $_SESSION['table'][$k][5]  = $time;
+//    if (isset($_SESSION[$k][0])) {
+//        $_SESSION['table'][$k][0] = 1;
+//        $_SESSION['table'][$k][1] = 2;
+//        $_SESSION['table'][$k][2] = 3;
+//        $_SESSION['table'][$k][3] = 4;
+//        $_SESSION['table'][$k][4] = 5;
+//        $_SESSION['table'][$k][5] = 6;
+//    } else {
+//        $_SESSION['table'][$k][0] = 'a';
+//        $_SESSION['table'][$k][1] = 'b';
+//        $_SESSION['table'][$k][2] = 'c';
+//        $_SESSION['table'][$k][3] = 'd';
+//        $_SESSION['table'][$k][4] = 'e';
+//        $_SESSION['table'][$k][5] = 'f';
+//
+//    }
+//    $_SESSION['table'][2][0] = 1;
+//    $_SESSION['table'][2][1]  = 2;
+//    $_SESSION['table'][2][2]  = 3;
+//    $_SESSION['table'][2][3]  = 4;
+//    $_SESSION['table'][2][4]  = 5;
+//    $_SESSION['table'][2][5]  = 6;
+    $attempt = array('x' => $x, 'y' => $y, 'r' => $r, 'result' => $answer, 'current_time' => $date, 'execution_time' => $time, 5);
+    //if ($r != 0) {
+        array_push($_SESSION['table'], $attempt);
+    //}
+//    else if($_SESSION['k'] <= 1) {
+//        foreach ($_SESSION['table'] as $values) {
+//            $chet = 0;
+//            foreach ($values as $value) {
+//                $chet++;
+//                //echo $value, "/";
+//                if ($chet >= 6) {
+//                    echo $value;
+//                } else {
+//                    echo $value, "/";
+//                }
+//            }
+//            echo ";";
+//        }
+//    }
+        //else{
+
+        foreach ($_SESSION['table'] as $values) {
+            $chet = 0;
+            foreach ($values as $value) {
+                $chet++;
+                //echo $value, "/";
+                if ($chet >= 6) {
+                    echo $value;
+                } else {
+                    echo $value, "/";
+                }
+            }
+            echo ";";
+        }
+    }
+
+    foreach ($_SESSION['table'] as $values) {
+        $chet = 0;
+        foreach ($values as $value) {
+            $chet++;
+            //echo $value, "/";
+            if ($chet >= 6) {
+                echo $value;
+            } else {
+                echo $value, "/";
+            }
+        }
+        echo ";";
+    //}
+
+
+//$chet = 0;
+//    if (isset($_SESSION['table'])) {
+//        foreach ($_SESSION['table'] as $item => $itemAr) {
+//            $chet = 0;
+//            foreach ($itemAr as $attr => $value) {
+//                $chet++;
+////                echo "[$item][$attr] = [$value]";
+//                if($chet >= 6){
+//                    echo $value;
+//                }
+//                else {
+//                    echo $value, "/";
+//                }
+
+
+////                echo $elem['x'], '/';
+////                echo $elem['y'], '/';
+////                echo $elem['r'], '/';
+////                echo $elem['result'], '/';
+////                echo $elem['current_time'], '/';
+////                echo $elem['execution_time'], ';';
+//            }
+//            echo ";";
+//        }
+//    }
+
+
+//    if (isset($_SESSION['table'])){
+//        for($i = 1; $i <= $k; $i++) {
+//            echo $_SESSION['table'][$i][0], '/';
+//            echo $_SESSION['table'][$i][1], '/';
+//            echo $_SESSION['table'][$i][2], '/';
+//            echo $_SESSION['table'][$i][3], '/';
+//            echo $_SESSION['table'][$i][4], '/';
+//            echo $_SESSION['table'][$i][5], ';';
+////            echo $i, '/';
+////            echo $i, '/';
+////            echo $i, '/';
+////            echo $i, '/';
+////            echo $i, '/';
+////            echo $i, ';';
+//        }
+
+    //table restore
+    //submit.js
+
+
+//        echo 1, '/';
+//        echo 2, '/';
+//        echo 3, '/';
+//        echo 4, '/';
+//        echo 5, '/';
+//        echo 6, ';';
+
+
+//    for($i = 0; $i < $k; $i++){
+//        for($j = 0; $j < 6; $j++){
+//            echo $_SESSION['table'][$k][$j], '/';
+//        }
+//        echo ";";
+//    }
+//    }
+
+//короче тут подписать передачу сессию в цикле каждую строку с нуля
 //echo $_SESSION['shot'];
 //$_SESSION['test']=$shot;
 //echo $_SESSION['test'];
@@ -72,13 +220,11 @@ else {
 //$new_url = "index.html";
 //header('Location: '.$new_url);
 
-/*$_SESSION['sao'] = '1';
-$_SESSION['qwe'] = 'Tutsplus';
-echo $_SESSION['sao'];
-echo $_SESSION['qwe'];
-*/
-?>
+    /*$_SESSION['sao'] = '1';
+    $_SESSION['qwe'] = 'Tutsplus';
+    echo $_SESSION['sao'];
+    echo $_SESSION['qwe'];
+    */
+}
 
-//обновление страницы
-//длинная арифметика
-//последов серв валидации
+?>
--- updatetable.js
+++ updatetable.js
@@ -58,12 +58,21 @@ zfield5.addEventListener("change", function () {
 
 //мегакривой выбор R
 
+// $(document).ready(function(){
+//     $.get("checkbox.php", {
+//         'state': 1
+//     }).done(function (data) {
+//         $('#result-table tr:first').after(data);
+//     });
+// });
+
 
 function showError(message) {
     $('#errors').append(`<li text-align: center >${message}</li>`);
 }
 
 function takeRequest(status, text) {
+    console.log('#');
     console.log(text);
     let Answer = text;
     if (Answer == "number_is_not_numeric") {
@@ -74,41 +83,159 @@ function takeRequest(status, text) {
         cell_x.innerHTML = "number_is_not_numeric";
         row.appendChild(cell_x);
     } else {
-        let arr = [6];
+        let arr = [];
+        let arrtable = [];
         for (i = 0; i < 6; i++) {
             arr[i] = "";
         }
-        let k = 0;
-        while (k < 6) {
-            for (i = 0; i < Answer.length; i++) {
-                if (Answer[i] == '/') {
-                    k++;
-                } else {
-                    arr[k] = arr[k] + Answer[i];
-                }
+        //// 1 версия
+        // let k = 0;
+        // let stopline = 0;
+        // while (k < 6) {
+        //     for (i = 0; i < Answer.length; i++) {
+        //         if(Answer[i] ==';'){
+        //             stopline = i;
+        //             k=7;
+        //             break;
+        //         }
+        //         else if (Answer[i] == '/') {
+        //             k++;
+        //         } else {
+        //             arr[k] = arr[k] + Answer[i];
+        //         }
+        //     }
+        // }
+        // arrtable.push([arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]]);
+        // stopline++;
+        // for (i = 0; i < 6; i++) {
+        //     arr[i] = "";
+        // }
+        // console.log(Answer[stopline]);
+        // k = 0;
+        // while (k < 6) {
+        //     for (i = stopline; i < Answer.length; i++) {
+        //         if(Answer[i] ==';'){
+        //             stopline = i;
+        //             k=7;
+        //         }
+        //         else if (Answer[i] == '/') {
+        //             k++;
+        //         } else {
+        //             arr[k] = arr[k] + Answer[i];
+        //         }
+        //     }
+        // }
+        // arrtable.push([arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]]);
+
+        //////////////////////////////////////////////////////////////
+
+
+        ////////////////2 версия
+        // let requestln = 0;
+        // let requestsnumber = "";
+        // for (i = 0; i < Answer.length; i++) {
+        //     if(Answer[i] != '!') {
+        //         requestsnumber = requestsnumber + Answer[i];
+        //     }
+        //     else{
+        //         requestln = i;
+        //         break;
+        //     }
+        // }
+        // console.log(requestsnumber);
+        // let k = 0;
+        // let stopline = requestln + 1;
+        // let k1 = 0;
+        // while (k1 < 2) {
+        //     while (k < 6) {
+        //         for (i = stopline; i < Answer.length; i++) {
+        //             if (Answer[i] == ';') {
+        //                 stopline = i + 1;
+        //                 k = 7;
+        //                 break;
+        //             } else if (Answer[i] == '/') {
+        //                 k++;
+        //             } else {
+        //                 arr[k] = arr[k] + Answer[i];
+        //             }
+        //         }
+        //     }
+        //     arrtable.push([arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]]);
+        //     k=0;
+        //     k1++;
+        //     for (i = 0; i < 6; i++) {
+        //         arr[i] = "";
+        //     }
+        // }
+
+
+        ///////////////////////////////////////////////////////////////////////////
+        let requestln = 0;
+        var requestsnumber = "";
+        for (i = 0; i < Answer.length; i++) {
+            if (Answer[i] != '!') {
+                requestsnumber = requestsnumber + Answer[i];
+            } else {
+                requestln = i;
+                break;
             }
         }
-        if (arr[0] == "61") {
-            arr[0] = "Попал";
-        } else {
-            arr[0] = "Не попал!";
+        console.log("Номер запроса", requestsnumber);
+        requestsnumber = Number(requestsnumber);
+        let k = 0;
+        let stopline = requestln + 1;
+        let k1 = 1;
+        while (k1 <= requestsnumber) {
+            while (k < 6) {
+                for (i = stopline; i < Answer.length; i++) {
+                    if (Answer[i] == ';') {
+                        stopline = i + 1;
+                        k = 7;
+                        break;
+                    } else if (Answer[i] == '/') {
+                        k++;
+                    } else {
+                        arr[k] = arr[k] + Answer[i];
+                    }
+                }
+            }
+            arrtable.push([arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]]);
+            console.log(arr[0]);
+            console.log(arr[1]);
+            console.log(arr[2]);
+            console.log(arr[3]);
+            console.log(arr[4]);
+            console.log(arr[5]);
+            console.log(k1, requestsnumber);
+            console.log(k1 + requestsnumber);
+            k = 0;
+            k1++;
+            for (i = 0; i < 6; i++) {
+                arr[i] = "";
+            }
         }
+        /////////////////////////////////////////////////////////////////////////////
+
+        console.log(arrtable);
+
+        $("tbody").empty();
 
         const table = document.getElementById("result-table");
         const tbody = table.getElementsByTagName("tbody")[0];
         const row = document.createElement("tr");
-        const cell_x = document.createElement("td");
-        const cell_y = document.createElement("td");
-        const cell_R = document.createElement("td");
-        const cell_hit = document.createElement("td");
-        const cell_request_time = document.createElement("td");
-        const cell_timing = document.createElement("td");
-        cell_x.innerHTML = arr[1];
-        cell_y.innerHTML = arr[2];
-        cell_R.innerHTML = arr[3];
-        cell_hit.innerHTML = arr[0];
-        cell_request_time.innerHTML = arr[4];
-        cell_timing.innerHTML = arr[5];
+        const cell_x = document.createElement("th");
+        const cell_y = document.createElement("th");
+        const cell_R = document.createElement("th");
+        const cell_hit = document.createElement("th");
+        const cell_request_time = document.createElement("th");
+        const cell_timing = document.createElement("th");
+
+        cell_x.innerHTML = 'X';
+        cell_y.innerHTML = 'Y';
+        cell_R.innerHTML = 'R';
+        cell_hit.innerHTML = 'Результат';
+        cell_request_time.innerHTML = "Время исполнения";
+        cell_timing.innerHTML = "Попадание";
         row.appendChild(cell_x);
         row.appendChild(cell_y);
         row.appendChild(cell_R);
@@ -116,21 +243,48 @@ function takeRequest(status, text) {
         row.appendChild(cell_request_time);
         row.appendChild(cell_timing);
         tbody.appendChild(row);
-        for (i = 0; i < 6; i++) {
-            console.log(arr[i]);
-        }
-        // for (let j in arr) {
-        //     const element = document.getElementById(j);
-        //     if (element) {
-        //         element.innerHTML = (arr[j]);
-        //     }
-        // };
 
-
-        //generateRowFromElem(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
-        //submit();
+        //localStorage.setItem("myKey",JSON.stringify(arr));
+        arrtable.forEach((element) => {
+            console.log(element)
+            const table = document.getElementById("result-table");
+            const tbody = table.getElementsByTagName("tbody")[0];
+            const row = document.createElement("tr");
+            const cell_x = document.createElement("td");
+            const cell_y = document.createElement("td");
+            const cell_R = document.createElement("td");
+            const cell_hit = document.createElement("td");
+            const cell_request_time = document.createElement("td");
+            const cell_timing = document.createElement("td");
+            cell_x.innerHTML = element[0];
+            cell_y.innerHTML = element[1];
+            cell_R.innerHTML = element[2];
+            cell_hit.innerHTML = element[3];
+            cell_request_time.innerHTML = element[4];
+            cell_timing.innerHTML = element[5];
+            row.appendChild(cell_x);
+            row.appendChild(cell_y);
+            row.appendChild(cell_R);
+            row.appendChild(cell_hit);
+            row.appendChild(cell_request_time);
+            row.appendChild(cell_timing);
+            tbody.appendChild(row);
+        })
+        // for (i = 0; i < 6; i++) {
+        //     console.log(arr[i]);
+        // }
     }
 }
+
+    // $(document).ready(function () {
+    //     let xhr = new XMLHttpRequest();
+    //     xhr.open("GET", "checkbox.php?x=" + 0 + "&y=" + 0 + "&r=" + 0
+    //         + "&startTime=" + new Date().getTime() + '&timeZone=' + new Date().getTimezoneOffset());
+    //     xhr.onloadend = () => {
+    //         takeRequest(xhr.status, xhr.responseText);
+    //     }
+    //     xhr.send();
+    // });
 
 
 var button = document.getElementById("button");
@@ -138,7 +292,7 @@ button.addEventListener("click", function (e) {
     $('#errors').empty();
     if (isNaN(parseFloat(y))) {
         showError("Ошибка: Y не является числом");
-    } else if (y.length >= 18 && (y[0] == 5 || y[1] == 5)){
+    } else if (y.length >= 18 && (y[0] == 5 || y[1] == 5)) {
         showError("Ведите число с меньшим количеством цифр после запятой");
     } else {
         if (y > 5 || y < -5) {
@@ -146,7 +300,7 @@ button.addEventListener("click", function (e) {
         } else {
             e.preventDefault();
             let xhr = new XMLHttpRequest();
-            xhr.open("get", "checkbox.php?x=" + x + "&y=" + y + "&r=" + r
+            xhr.open("GET", "checkbox.php?x=" + x + "&y=" + y + "&r=" + r
                 + "&startTime=" + new Date().getTime() + '&timeZone=' + new Date().getTimezoneOffset());
             xhr.onloadend = () => {
                 takeRequest(xhr.status, xhr.responseText);
